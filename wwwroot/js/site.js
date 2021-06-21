fc05d5e166abdf3207e734c2d0cf2f30d778f93a
GustavoBorges <sgustavo.borges10@gmail.com>
2021-06-21 11:46:28 -0300

    Design aplicado e funcionando MVC

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function botaoDoMenu() {
+    var menu = document.getElementById("links");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none"
+        // esconder menu
+    } else{
+        menu.style.display = "flex"
+        // mostre menu
+    }
+}
